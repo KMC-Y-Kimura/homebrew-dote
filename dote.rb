a4d40956437241e4d9e3c5001d09731a6b4b8608
Yuta Kimura <kmc2517@kamiyama.ac.jp>
2025-10-28 11:04:53 +0900

    version 1.0.0

--- dote.rb
+++ dote.rb
@@ -2,7 +2,8 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/KMC-Y-Kimura/homebrew-dote"
   url "https://raw.githubusercontent.com/KMC-Y-Kimura/homebrew-dote/main/dote.c"
-  sha256 "PUT_SHA256_OF_DOTE.C_HERE"
+  sha256 "e1f50b86cb593b356772a95b4000e1cb8781868bd4e5cc0566af6aec95efeef2"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
